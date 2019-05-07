a477c51ccc75f396995645d2742da9421107463a
realkyr <60070075@kmitl.ac.th>
2019-05-07 22:18:57 +0700

    :sparkles: add Review Feature

---
+++ pages/reviews/add/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <div id="editorjs"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {}
+  },
+  mounted() {
+    const EditorJS = require('@editorjs/editorjs')
+    let editor = new EditorJS({ // eslint-disable-line
+      holderId: 'editorjs',
+      tools: {
+        header: {
+          class: Header, // eslint-disable-line
+          inlineToolbar: true
+        }
+      }
+    })
+  }
+}
+</script>
